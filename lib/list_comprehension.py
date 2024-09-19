ec80a116b7409a9f97f499dada38ce05f8a25362
ni9216 <nicholasliech851@gmail.com>
2024-09-19 12:14:27 +0300

    commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python3
 
 def return_evens(num_list):
+    list = [i for i in num_list if i%2==0]
+    return list
     pass
 
 def make_exclamation(sentence_list):
+    if sentence_list == []:
+        return []
+    generator = (eachsentence + "!" for eachsentence in sentence_list)
+    return list(generator)
     pass
